518a2fe2db8514dc167a8223e4f3790b8e6ebac2
RaziyaM <97949320+RaziyaM@users.noreply.github.com>
2022-10-31 15:33:41 +0200

    :)

---
+++ headers/function/built_binary.h
@@ -0,0 +1,10 @@
+char * int_to_bin(int num, int bits_len);
+char * return_reg_binary(char * reg);
+char * return_opcode_binary_str(char * _opcode);
+int return_opcode_index(char * opcode);
+char * convert_my_base32(char * binary_str);
+int from_bin_to_dec(char * bin_num);
+
+char * return_imm_num_binary(char * operand);
+char * return_struct_index_binary(char * index);
+char * return_struct_binary(char * operand);
